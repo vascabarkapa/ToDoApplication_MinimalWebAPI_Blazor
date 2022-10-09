629aa56dcd0d7af2e7f0898fe2e94ca342b6dc6d
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-10-09 20:29:17 +0200

    todoitem service

---
+++ Client/Services/TodoItemService/ITodoItemService.cs
@@ -0,0 +1,17 @@
+﻿namespace Client.Services.TodoItemService
+{
+    public interface ITodoItemService
+    {
+        List<TodoItem> TodoItems { get; set; }
+
+        Task GetTodoItems();
+
+        Task<TodoItem> GetSingleTodoItem(int id);
+
+        Task CreateTodoItem(TodoItem todoItem);
+
+        Task UpdateTodoItem(TodoItem todoItem);
+
+        Task DeleteTodoItem(int id);
+    }
+}
---
+++ Client/Services/TodoItemService/TodoItemService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Client.Services.TodoItemService
+{
+    public class TodoItemService : ITodoItemService
+    {
+        // https://localhost:7019/
+
+        private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
+
+        public TodoItemService(HttpClient http, NavigationManager navigationManager)
+        {
+            _http = http;
+            _navigationManager = navigationManager;
+        }
+
+        public List<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
+
+        public async Task CreateTodoItem(TodoItem todoItem)
+        {
+            var result = await _http.PostAsJsonAsync("api/todoItem", todoItem);
+            var response = await result.Content.ReadFromJsonAsync<List<TodoItem>>();
+
+            if (response != null)
+            {
+                TodoItems = response;
+                _navigationManager.NavigateTo("todoItems");
+            }
+
+            throw new Exception("Unexpected error on adding new Task");
+        }
+
+        public async Task UpdateTodoItem(TodoItem todoItem)
+        {
+            var result = await _http.PutAsJsonAsync($"api/todoItem/{todoItem.Id}", todoItem);
+            var response = await result.Content.ReadFromJsonAsync<List<TodoItem>>();
+
+            if (response != null)
+            {
+                TodoItems = response;
+                _navigationManager.NavigateTo("todoItems");
+            }
+
+            throw new Exception("Unexpected error on adding new Task");
+        }
+
+        public async Task DeleteTodoItem(int id)
+        {
+            var result = await _http.DeleteAsync($"api/todoItem/{id}");
+            var response = await result.Content.ReadFromJsonAsync<List<TodoItem>>();
+
+            if (response != null)
+            {
+                TodoItems = response;
+                _navigationManager.NavigateTo("todoItems");
+            }
+
+            throw new Exception("Unexpected error on adding new Task");
+        }
+
+        public async Task<TodoItem> GetSingleTodoItem(int id)
+        {
+            var response = await _http.GetFromJsonAsync<TodoItem>($"api/todoItem/{id}");
+
+            if (response != null)
+            {
+                return response;
+            }
+
+            throw new Exception("To Do not found!");
+        }
+
+        public async Task GetTodoItems()
+        {
+            var response = await _http.GetFromJsonAsync<List<TodoItem>>("https://localhost:7019/api/todoItem");
+
+            if (response != null)
+            {
+                TodoItems = response;
+            }
+        }
+    }
+}
