5ea34b8c31d85a1d49e7f0d04dde0856f7e470b2
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-10-09 14:54:30 +0200

    CRUD Min APIs

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Server;
+using Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,5 +22,59 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+async Task<List<TodoItem>> GetTodoItems(ApplicationDbContext _db) =>
+    await _db.TodoItems.ToListAsync();
+
+app.MapGet("/api/todoItem", async (ApplicationDbContext _db) =>
+    await _db.TodoItems.ToListAsync());
+
+app.MapGet("/api/todoItem/{id}", async (ApplicationDbContext _db, int id) =>
+    await _db.TodoItems.FindAsync(id) is TodoItem todoItem ?
+    Results.Ok(todoItem) :
+    Results.NotFound("There is not To Do Item!"));
+
+app.MapPost("/api/todoItem", async (ApplicationDbContext _db, TodoItem todoItem) =>
+{
+    _db.TodoItems.Add(todoItem);
+    await _db.SaveChangesAsync();
+
+    return Results.Ok(await GetTodoItems(_db));
+});
+
+app.MapPut("/api/todoItem/{id}", async (ApplicationDbContext _db, TodoItem todoItem, int id) =>
+{
+    var dbTodoItem = await _db.TodoItems.FindAsync(id);
+
+    if (dbTodoItem == null)
+    {
+        return Results.NotFound("There is not To Do Item!");
+    }
+
+    dbTodoItem.Name = todoItem.Name;
+    dbTodoItem.Description = todoItem.Description;
+    dbTodoItem.DateAndTime = todoItem.DateAndTime;
+    dbTodoItem.Priority = todoItem.Priority;
+    dbTodoItem.isDone = todoItem.isDone;
+
+    await _db.SaveChangesAsync();
+
+    return Results.Ok(await GetTodoItems(_db));
+});
+
+app.MapDelete("/api/todoItem/{id}", async (ApplicationDbContext _db, int id) =>
+{
+    var dbTodoItem = await _db.TodoItems.FindAsync(id);
+
+    if (dbTodoItem == null)
+    {
+        Results.NotFound("There is not To Do Item!");
+    }
+
+    _db.TodoItems.Remove(dbTodoItem);
+    await _db.SaveChangesAsync();
+
+    return Results.Ok(await GetTodoItems(_db));
+});
 
 app.Run();
